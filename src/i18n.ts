1656bde8a446de721a1ac69f8691e4005fe0d3b3
HealthJoy Home Care <info@healthjoy.com.au>
2026-03-02 13:07:07 +1100

    feat: implement automatic language detection based on browser settings

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enTranslation from './locales/en/translation.json';
 import zhTranslation from './locales/zh/translation.json';
@@ -8,6 +9,7 @@ import jaTranslation from './locales/ja/translation.json';
 import koTranslation from './locales/ko/translation.json';
 
 i18n
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: {
@@ -27,8 +29,11 @@ i18n
                 translation: koTranslation,
             },
         },
-        lng: 'en', // default language
         fallbackLng: 'en',
+        detection: {
+            order: ['localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+            caches: ['localStorage'],
+        },
         interpolation: {
             escapeValue: false, // react already safes from xss
         },
